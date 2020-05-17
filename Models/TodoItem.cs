60036cff7ff6d547a44dc5aa6fa69a516fad208e
Cristina <cristina.ilea2@yahoo.com>
2020-05-17 15:46:03 +0300

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Models
+{
+    public enum Importance { Low, Medium, High }
+    public enum State { Open, InProgress, Closed }
+
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset AddedDate { get; set; }
+        public DateTimeOffset Deadline { get; set; }
+        public Importance Importance { get; set; }
+        public State State { get; set; }
+        public DateTimeOffset ClosedDate { get; set; }
+    }
+}
